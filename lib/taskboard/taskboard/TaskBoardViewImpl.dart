81dca5e4d58601762e385cbb9effb35ee471203c
mrtn <mno13@webmail.tu-clausthal.de>
2018-01-07 01:03:23 +0100

    added home screen logic

--- lib/taskboard/taskboard/TaskBoardViewImpl.dart
+++ lib/taskboard/taskboard/TaskBoardViewImpl.dart
@@ -6,13 +6,21 @@ import 'package:taskr_flutter/taskboard/taskboard/TaskBoardPresenterImpl.dart';
 
 class TaskBoardView extends TaskBoardViewContract {
   TaskBoardPresenter _presenter;
+  _TaskBoardListState listState;
+  List<TaskBoard> data;
 
   TaskBoardView() {
+    data = new List();
     this._presenter = new TaskBoardPresenter(this);
   }
 
   @override
   void displayTaskBoards(List<TaskBoard> data) {
+    listState.updateData(data);
+  }
+
+  void getTaskBoards() {
+    _presenter.getTaskBoards();
   }
 
   @override
@@ -28,23 +36,104 @@ class TaskBoardView extends TaskBoardViewContract {
   void onTaskBoardJoined(bool result, {String msg}) {
   }
 
-  Widget createContent() {
-    return new ListView(
-
+  Widget getLoadingScreen() {
+    return new Center(
+      child: new Container(
+          color: Colors.blue,
+          child: new Column(
+            children: <Widget>[
+              new Text('Please wait while we are setting up your TaskBoards.')
+            ],
+          )),
     );
   }
 
+  Card createTaskCard(String taskTitle, String taskContent, String date,
+      String participators) {
+    return new Card(
+      child: new Column(
+        children: <Widget>[
+          new ListTile(
+              title: new Text(taskTitle,
+                  style: new TextStyle(fontWeight: FontWeight.w500)),
+              subtitle: new Text(participators)
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget getContent() {
+    if (data.isEmpty)
+      return getEmptyContentScreen();
+    List<Widget> list = new List();
+    for (int i = 0; i < 3; i++) {
+      list.add(createTaskCard("Task $i", "Content $i", "000", "i" * i));
+    }
+    return new Center(
+        child: new ListView(
+          children: list,
+        )
+    );
+  }
 
   @override
-  TaskBoardState createState() {
-    return new TaskBoardState();
+  _TaskBoardListState createState() {
+    listState = new _TaskBoardListState(data);
+    return listState;
   }
+
+
+  Widget getEmptyContentScreen() {
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+
+        new Text(
+            'You have no TaskBoard signed up yet. Would you like to create or join a new TaskBoard?'),
+        new Row(
+          children: <Widget>[
+            new FlatButton(onPressed: null, child: new Text('Create')),
+            new FlatButton(onPressed: null, child: new Text('Join'))
+          ],
+        )
+      ],
+    );
+  }
+
 }
 
-class TaskBoardState extends State<TaskBoardView> {
+
+class _TaskBoardListState extends State<TaskBoardView> {
+  List<TaskBoard> data;
+  bool data_updated = false;
+
+  _TaskBoardListState(this.data);
+
+  void updateData(List<TaskBoard> data) {
+    this.setState(() {
+      print('Updating setState with data');
+      data_updated = true;
+      if (data.isEmpty)
+        return;
+      this.data.addAll(data);
+    });
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    widget.getTaskBoards();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new TaskrScaffold(title: 'TaskBoards', body: new Text('My friend?'));
+    return new TaskrScaffold(
+        title: 'TaskBoard',
+        body: data_updated ? new Container(
+            child: new Expanded(child: new Container(child: widget.getContent()))) : widget
+            .getLoadingScreen());
   }
+
 }
--- lib/util/Converter.dart
+++ lib/util/Converter.dart
@@ -4,6 +4,7 @@ import 'package:taskr_flutter/data/TaskFactory.dart';
 import 'dart:convert';
 
 class Converter {
+
   static String taskToJson(Task task) {
     Map data = new Map();
     data[Task.ID] = task.id;
@@ -15,6 +16,11 @@ class Converter {
   }
 
   static String taskBoardToJson(TaskBoard board) {
+    /**
+     * Custom Json Data:
+     * {"title": "Family Nowosad", "description": "This is our personal TaskBoard",
+     * "secret": "secret password", "public_id": "public_id"
+     */
     Map data = new Map();
     List<String> taskJsonList = new List();
     for (Task t in board.tasks) {
@@ -23,16 +29,29 @@ class Converter {
     }
     data[TaskBoard.SECRET] = board.secret;
     data[TaskBoard.TITLE] = board.title;
-    data[TaskBoard.TASKS] = JSON.encode(taskJsonList);
     data[TaskBoard.ID] = board.id;
+    data[TaskBoard.DESCRIPTION] = board.description;
+    data[TaskBoard.PUBLIC_KEY] = board.publicKey;
     return JSON.encode(data);
   }
 
   static TaskBoard jsonToTaskBoard(String taskboardJson) {
-    String secret, title;
+    String secret, title, publicKey, description;
+    int id, maxTasks;
+    DateTime createdOn, lastUpdate;
+
     Map json = JSON.decode(taskboardJson);
+
     secret = json[TaskBoard.SECRET];
     title = json[TaskBoard.TITLE];
+    publicKey = json[TaskBoard.PUBLIC_KEY];
+    description = json[TaskBoard.DESCRIPTION];
+    publicKey = json[TaskBoard.PUBLIC_KEY];
+    id = json[TaskBoard.ID];
+    maxTasks = json[TaskBoard.MAX_TASKS];
+    createdOn = json[TaskBoard.CREATED_ON];
+    lastUpdate = json[TaskBoard.LAST_UPDATE];
+
     return TaskFactory.createTaskBoard(secret, title, null);
   }
 
@@ -41,7 +60,7 @@ class Converter {
     return json;
   }
 
-  static Map parseTaskBoardJsonAsMap(String taskJson){
+  static Map parseTaskBoardJsonAsMap(String taskJson) {
     Map json = JSON.decode(taskJson);
     return json;
   }
